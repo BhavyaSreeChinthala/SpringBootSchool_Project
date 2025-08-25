5ce788135024a73282192b57ee6938789a8953fd
Bhavya Sree Chinthala <bhavyasree.chinthala@Bhavyas-MacBook-Air.local>
2025-08-25 18:25:36 -0500

    Implementing Swagger Document for School API

---
+++ src/main/java/com/example/project1/security/OpenApiConfig.java
@@ -0,0 +1,30 @@
+package com.example.project1.security;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("School API")
+                        .version("1.0")
+                        .description("API with JWT Authentication"))
+                .addSecurityItem(new SecurityRequirement().addList("Bearer"))
+                .components(new Components().addSecuritySchemes("Bearer",
+                        new SecurityScheme()
+                                .name("Authorization")
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                                .in(SecurityScheme.In.HEADER)));
+    }
+}
+
